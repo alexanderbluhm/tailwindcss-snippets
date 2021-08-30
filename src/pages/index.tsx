ef33e092a046b9e53241b37ebad7bb1c27d88e6a
Alexander Bluhm <lamiy-develop@live.de>
2021-08-30 23:17:26 +0200

    Enhance svg docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,8 +18,8 @@ const featuredShorts = [
 ];
 
 const gradients = [
-    "bg-gradient-to-r from-yellow-300 to-yellow-500",
-    "bg-gradient-to-r from-green-300 to-emerald-500",
+    "bg-gradient-to-r from-yellow-200 to-yellow-400",
+    "bg-gradient-to-r from-green-200 to-emerald-400",
     "bg-gradient-to-br from-blue-400 to-cyan-500",
 ]
 
@@ -27,19 +27,28 @@ const Home = (props: Props) => {
   return (
     <BasicLayout>
       <div className="max-w-[65ch] mx-auto">
-        <h1 className="text-4xl font-bold text-center bg-clip-text bg-gradient-to-br text-transparent from-cyan-400 to-indigo-400">TailwindCSS Shorts</h1>
-        <h2 className="mt-8 text-xl font-medium">A collection of useful TailwindCSS content including tricks, simple code examples and explanations.</h2>
+        <h1 className="text-3xl font-bold text-center text-black">TailwindCSS Shorts</h1>
+        <h2 className="mt-8 text-lg">A collection of useful TailwindCSS content including tricks, simple code examples and explanations.</h2>
       </div>
-      <section aria-labelledby="featured-snippets" className="mt-8 max-w-[65ch] mx-auto grid grid-cols-3 gap-8">
+      <section aria-labelledby="featured-snippets" className="mt-8 max-w-[65ch] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <h2 className="sr-only">Featured Shorts</h2>
         {featuredShorts.map((snippet, i) => (
           <Link href={`/${snippet.slug}`}>
-            <a className={`${gradients[i]} text-white rounded-md p-4 space-y-2`} key={snippet.slug}>
+            <a className={`${gradients[i]} text-black rounded-md p-4 space-y-2`} key={snippet.slug}>
               <h3 className="text-lg font-medium">{snippet.title}</h3>
               <p className="underline">{snippet.description}</p>
             </a>
           </Link>
         ))}
+      </section>
+
+      <section>
+          {/* TODO:
+         - variant plugin
+         - methods to center (flex, translate)
+         - not abuse jit mode because of design system
+         - sticky navbar and sticky footer
+         - usage of truncate */}
       </section>
     </BasicLayout>
   );
