21762126fa939d1caead5f45c93d0fc1d39519f0
Alexander Bluhm <info@alexanderbluhm.de>
2021-10-20 10:52:30 +0200

    Add print example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  darkMode: 'media',
+  darkMode: "media",
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
-    "./remark/**/*.js"
+    "./remark/**/*.js",
   ],
   theme: {
-    colors: {...colors, "transparent": "transparent"},
-    extend: {},
+    colors: { ...colors, transparent: "transparent" },
+    extend: {
+      screens: {
+        print: { raw: "print" },
+      },
+    },
   },
   variants: {
     extend: {},
